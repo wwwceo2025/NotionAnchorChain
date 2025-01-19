0b3e29d34d3f46381ed304faea3e971dbd25b8d4
wwwceo2025 <wwwceo@gmail.com>
2025-01-19 11:59:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@
 const BLOG = {
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-    '175a0a9b2ff180d28857cf9ba0e25010,en:175a0a9b2ff180d28857cf9ba0e25010',
+    '180d2d35fb318067b332d184ee05e3f9,en:180d2d35fb318067b332d184ee05e3f9',
   THEME: process.env.NEXT_PUBLIC_THEME || 'starter', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'en-US', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2025, // e.g if leave this empty, current year will be used.
